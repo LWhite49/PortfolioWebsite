f947dd8a706279a12c52d567b70625f15ca8c8ab
Logan White <135063791+LWhite49@users.noreply.github.com>
2024-06-21 23:45:00 -0400

    AeroBubbles Component Preset

---
+++ src/AeroBubbles/AeroBubbles.jsx
@@ -0,0 +1,99 @@
+import "./AeroBubbles.css";
+import { useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+// import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
+// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
+
+export const AeroBubbles = () => {
+	const [init, setInit] = useState(false);
+
+	// this should be run only once per application lifetime
+	useEffect(() => {
+		initParticlesEngine(async (engine) => {
+			// you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+			// this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+			// starting from v2 you can add only the features you need reducing the bundle size
+			//await loadAll(engine);
+			//await loadFull(engine);
+			await loadSlim(engine);
+			//await loadBasic(engine);
+		}).then(() => {
+			setInit(true);
+		});
+	}, []);
+
+	const particlesLoaded = (container) => {
+		console.log(container);
+	};
+
+	return init ? (
+		<Particles
+			className="AeroBubbles"
+			id="tsparticles"
+			particlesLoaded={particlesLoaded}
+			options={{
+				background: {
+					color: {
+						value: "#ebf2e4",
+					},
+				},
+				fpsLimit: 120,
+				interactivity: {
+					events: {
+						resize: true,
+					},
+					modes: {
+						push: {
+							quantity: 4,
+						},
+						repulse: {
+							distance: 40,
+							duration: 0.4,
+						},
+					},
+				},
+				particles: {
+					color: {
+						value: "#348500",
+					},
+					links: {
+						color: "#2b7deb",
+						distance: 200,
+						enable: true,
+						opacity: 0.5,
+						width: 2,
+					},
+					move: {
+						direction: "none",
+						enable: true,
+						outModes: {
+							default: "bounce",
+						},
+						random: true,
+						speed: 3,
+						straight: false,
+					},
+					number: {
+						density: {
+							enable: true,
+							area: 1200,
+						},
+						value: 70,
+					},
+					opacity: {
+						value: 0.5,
+					},
+					shape: {
+						type: "circle",
+					},
+					size: {
+						value: { min: 1, max: 3 },
+					},
+				},
+				detectRetina: true,
+			}}
+		/>
+	) : null;
+};
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,10 +1,12 @@
 import "./Home.css";
 import LWImg from "../assets/LWImg.jpg";
+import { AeroBubbles } from "../AeroBubbles/AeroBubbles.jsx";
 
 export const Home = () => {
 	return (
 		<div className="Home">
-			<h1 className="Home-Intro-Text">Hello!</h1>
+			<h1 className="Home-Intro-Text">Developer Portfolio</h1>
+			<AeroBubbles />
 			<img className="Personal-Image" src={LWImg} alt="LWImg" />
 		</div>
 	);
