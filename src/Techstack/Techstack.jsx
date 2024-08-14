42929fe1c8beeacd6850bb2a67c5979f813d55b0
Logan White <135063791+LWhite49@users.noreply.github.com>
2024-08-14 15:58:59 -0400

    Tech Stack Color Pallet

--- src/Techstack/Techstack.jsx
+++ src/Techstack/Techstack.jsx
@@ -1,9 +1,21 @@
 import "./Techstack.css";
-
+import { useContext } from "react";
+import { AppContext } from "../App.jsx";
+import Apple from "../assets/Apple.gif";
 export const Techstack = () => {
+	const { MemoizedAeroBubbles } = useContext(AppContext);
 	return (
 		<div className="Techstack">
-			<h1>Techstack</h1>
+			<MemoizedAeroBubbles
+				background="#DCF9F5"
+				node="#000000"
+				links="#5CAB7D"
+			/>
+			<div className="Techstack-Header-Wrapper">
+				<img className="Apple" src={Apple} />
+
+				<h1 className="Techstack-Header">Techstack Map</h1>
+			</div>
 		</div>
 	);
 };
