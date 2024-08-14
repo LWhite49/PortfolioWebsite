6908b0f002b996d4a45336ff51607765e7ff62bc
Logan White <135063791+LWhite49@users.noreply.github.com>
2024-08-14 18:37:50 -0400

    Linked Resume - PDFA

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -11,7 +11,7 @@ import ResumeSvg from "../assets/ResumeSvg.svg";
 import EmailSvg from "../assets/EmailSvg.svg";
 import LinkedInSvg from "../assets/LinkedInSvg.svg";
 import GithubSvg from "../assets/GithubSvg.svg";
-import Resume from "../assets/Resume.pdf";
+import Resume from "../assets/Logan White - Resume.pdf";
 
 export const Home = () => {
 	// Create style object for Tooltips
