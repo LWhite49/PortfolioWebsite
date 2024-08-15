6b829a2aa1c4ff69e4bc7d5fc0376d09fd08bf17
Logan White <135063791+LWhite49@users.noreply.github.com>
2024-08-15 16:07:31 -0400

    Tech Stack Header and Color

--- src/Techstack/Techstack.jsx
+++ src/Techstack/Techstack.jsx
@@ -2,19 +2,21 @@ import "./Techstack.css";
 import { useContext } from "react";
 import { AppContext } from "../App.jsx";
 import Apple from "../assets/Apple.gif";
+import Bowl from "../assets/Bowl.gif";
 export const Techstack = () => {
 	const { MemoizedAeroBubbles } = useContext(AppContext);
 	return (
 		<div className="Techstack">
 			<MemoizedAeroBubbles
-				background="#DCF9F5"
+				background="#FFC2AD"
 				node="#000000"
-				links="#5CAB7D"
+				links="#540552"
 			/>
 			<div className="Techstack-Header-Wrapper">
 				<img className="Apple" src={Apple} />
 
-				<h1 className="Techstack-Header">Techstack Map</h1>
+				<h1 className="Techstack-Header">Techstack</h1>
+				<img className="Bowl" src={Bowl} />
 			</div>
 		</div>
 	);
