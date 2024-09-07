68fe7bb62fefa61494aab8f34f58fb674a4714a8
Logan White <135063791+LWhite49@users.noreply.github.com>
2024-09-07 15:42:18 -0400

    Loading Screen finished - consider storing CSS styles for loading screens for each last page visited

--- src/Loading/Loading.jsx
+++ src/Loading/Loading.jsx
@@ -1,16 +1,29 @@
 import "./Loading.css";
 import { useContext } from "react";
 import { AppContext } from "../App.jsx";
-
+import loadingSpinner from "../assets/loadingSpinner.svg";
 export const Loading = () => {
 	// Source AeroBubbles component
-	const { MemoizedAeroBubbles } = useContext(AppContext);
+	const { windowWidth, MemoizedAeroBubbles } = useContext(AppContext);
 	return (
 		<>
-			<MemoizedAeroBubbles />
-			<div className="loading">
-				<div className="loading__container">
-					<div className="loading__text">Loading...</div>
+			<MemoizedAeroBubbles
+				background="#e6d6eb"
+				node="#e6d6eb"
+				links="#e6d6eb"
+			/>
+
+			<div className="Loading">
+				<div className="Loading-Content">
+					<p className="Loading-Text">Just a second!</p>
+					<img
+						className="Loading-Spinner"
+						src={loadingSpinner}
+						alt="logo"
+					/>
+					<p className="Loading-Text">
+						Load balancing is important...
+					</p>
 				</div>
 			</div>
 		</>
